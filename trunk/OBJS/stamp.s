d395e19df7f91e30d6ab72e09742a130c063a468
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-08-12 09:08:11 +0000

    Fixed issues 1, 2 and 3

--- trunk/OBJS/stamp.s
+++ trunk/OBJS/stamp.s
@@ -20,17 +20,17 @@ __zero_reg__ = 1
 	.type	stamp1, @object
 	.size	stamp1, 19
 stamp1:
-	.string	"VERS: V1.1790-erez"
+	.string	"VERS: V1.1801-erez"
 .global	stamp2
 	.type	stamp2, @object
 	.size	stamp2, 17
 stamp2:
-	.string	"DATE: 11.08.2010"
+	.string	"DATE: 12.08.2010"
 .global	stamp3
 	.type	stamp3, @object
 	.size	stamp3, 15
 stamp3:
-	.string	"TIME: 22:44:30"
+	.string	"TIME: 12:05:30"
 .global	stamp4
 	.type	stamp4, @object
 	.size	stamp4, 15
--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,4 +1,4 @@
-#define DATE_STR "11.08.2010"
-#define TIME_STR "22:44:30"
-#define SUB_VERS 1790-erez
+#define DATE_STR "12.08.2010"
+#define TIME_STR "12:05:30"
+#define SUB_VERS 1801-erez
 #define SVN_VERS "trunk-r5"
