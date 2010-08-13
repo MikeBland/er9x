6883c22089f3433da5ed139e1e28aeeea3c042a2
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-08-13 21:36:57 +0000

    housekeeping

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,4 +1,4 @@
-#define DATE_STR "13.08.2010"
-#define TIME_STR "23:05:44"
-#define SUB_VERS 1831-erez
-#define SVN_VERS "trunk-r10"
+#define DATE_STR "14.08.2010"
+#define TIME_STR "00:35:54"
+#define SUB_VERS 1832-erez
+#define SVN_VERS "trunk-r11"
