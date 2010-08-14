94b4b0220907937471b1d29753a987a3f95f564d
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-08-14 11:36:31 +0000

    Issues: 4, 17

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,4 +1,4 @@
 #define DATE_STR "14.08.2010"
-#define TIME_STR "12:32:11"
-#define SUB_VERS 1833-erez
-#define SVN_VERS "trunk-r12"
+#define TIME_STR "14:29:53"
+#define SUB_VERS 1843-erez
+#define SVN_VERS "trunk-r13"
