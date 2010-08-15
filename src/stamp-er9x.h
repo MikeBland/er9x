65b4dfc217ceaa190d7da5f7f03c4d08569aa405
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-08-15 18:31:46 +0000

    Issue 29

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,4 +1,4 @@
 #define DATE_STR "15.08.2010"
-#define TIME_STR "17:34:53"
-#define SUB_VERS 1889-erez
-#define SVN_VERS "trunk-r19"
+#define TIME_STR "21:26:29"
+#define SUB_VERS 1902-erez
+#define SVN_VERS "trunk-r20"
