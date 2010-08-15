0b9d125b7c2cce1a189ea16c4d01326648e275a6
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-08-15 19:29:53 +0000

    Midway on issue 11

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,4 +1,4 @@
 #define DATE_STR "15.08.2010"
-#define TIME_STR "21:26:29"
-#define SUB_VERS 1902-erez
-#define SVN_VERS "trunk-r20"
+#define TIME_STR "22:27:06"
+#define SUB_VERS 1908-erez
+#define SVN_VERS "trunk-r21"
