4869e63eb2c4f794b41c55fb4301007d5fc92c5d
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-08-16 21:26:15 +0000

    Hopefully working correctly now

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,4 +1,4 @@
-#define DATE_STR "16.08.2010"
-#define TIME_STR "22:51:35"
-#define SUB_VERS 1936-erez
-#define SVN_VERS "trunk-r25"
+#define DATE_STR "17.08.2010"
+#define TIME_STR "00:23:15"
+#define SUB_VERS 1947-erez
+#define SVN_VERS "trunk-r26"
