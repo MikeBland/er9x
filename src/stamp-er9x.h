ee7559f17f871abef14f861b45c6fad29e306981
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-08-23 17:54:11 +0000

    Fix to navigation

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,4 +1,4 @@
 #define DATE_STR "23.08.2010"
-#define TIME_STR "20:38:49"
-#define SUB_VERS 2115-erez
-#define SVN_VERS "trunk-r38"
+#define TIME_STR "20:53:52"
+#define SUB_VERS 2117-erez
+#define SVN_VERS "trunk-r39"
