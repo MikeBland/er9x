2432d880b4fa6c4e3d769371282c3eeb387e10f5
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-08-26 19:00:57 +0000

    Modelselect Fix

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,4 +1,4 @@
 #define DATE_STR "26.08.2010"
-#define TIME_STR "21:12:33"
-#define SUB_VERS 2207-erez
-#define SVN_VERS "trunk-r42"
+#define TIME_STR "21:58:48"
+#define SUB_VERS 2210-erez
+#define SVN_VERS "trunk-r43"
