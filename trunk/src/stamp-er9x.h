ed97c4b3e1444c18c7941e69ed6416fc2a03b24b
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-08-28 18:37:56 +0000

    Display updates

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,4 +1,4 @@
 #define DATE_STR "28.08.2010"
-#define TIME_STR "21:11:23"
-#define SUB_VERS 2265-erez
-#define SVN_VERS "trunk-r62"
+#define TIME_STR "21:35:25"
+#define SUB_VERS 2268-erez
+#define SVN_VERS "trunk-r63"
