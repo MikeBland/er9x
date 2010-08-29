c54de44ddcae3e9d6053b07a71188562355647d5
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-08-29 16:09:57 +0000

    Issue 67, ppm timing

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,4 +1,4 @@
 #define DATE_STR "29.08.2010"
-#define TIME_STR "00:02:18"
-#define SUB_VERS 2283-erez
-#define SVN_VERS "trunk-r66"
+#define TIME_STR "19:07:12"
+#define SUB_VERS 2286-erez
+#define SVN_VERS "trunk-r69"
