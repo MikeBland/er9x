c26e4a12d1a2052232b9cdf045633ce8dfe8434b
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-08-30 14:42:01 +0000

    fix for curves

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,4 +1,4 @@
 #define DATE_STR "30.08.2010"
-#define TIME_STR "17:33:03"
-#define SUB_VERS 2293-erez
-#define SVN_VERS "trunk-r78"
+#define TIME_STR "17:41:44"
+#define SUB_VERS 2294-erez
+#define SVN_VERS "trunk-r79"
