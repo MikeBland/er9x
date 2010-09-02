4feb79da94ab62d495bb5380eca41db8648170a8
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-09-02 14:12:45 +0000

    Issue 76, Issue 49

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,4 +1,4 @@
 #define DATE_STR "02.09.2010"
-#define TIME_STR "17:01:46"
-#define SUB_VERS 2413-erez
-#define SVN_VERS "trunk-r89"
+#define TIME_STR "17:08:46"
+#define SUB_VERS 2415-erez
+#define SVN_VERS "trunk-r90"
