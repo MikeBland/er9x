03d2261c2b580dc496e2076a4f7ea48e6fc84c35
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-09-03 12:17:04 +0000

    Issue 75, Issue 80, Issue 82

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,4 +1,4 @@
-#define DATE_STR "02.09.2010"
-#define TIME_STR "17:08:46"
-#define SUB_VERS 2415-erez
-#define SVN_VERS "trunk-r90"
+#define DATE_STR "03.09.2010"
+#define TIME_STR "15:15:46"
+#define SUB_VERS 2426-erez
+#define SVN_VERS "trunk-r91"
