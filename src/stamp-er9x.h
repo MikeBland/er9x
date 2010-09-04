6d96497c3997d4f46a290a4c7cc093caed1ac43f
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-09-04 16:54:51 +0000

    some work

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,4 +1,4 @@
 #define DATE_STR "04.09.2010"
-#define TIME_STR "19:32:34"
-#define SUB_VERS 2457-erez
-#define SVN_VERS "trunk-r92"
+#define TIME_STR "19:53:55"
+#define SUB_VERS 2458-erez
+#define SVN_VERS "trunk-r93"
