ae286e10001255d683d8bdca578872a8213c718d
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-09-05 20:34:24 +0000

    mid work

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,4 +1,4 @@
 #define DATE_STR "05.09.2010"
-#define TIME_STR "18:44:45"
-#define SUB_VERS 2462-erez
-#define SVN_VERS "trunk-r95"
+#define TIME_STR "23:08:36"
+#define SUB_VERS 2472-erez
+#define SVN_VERS "trunk-r96"
