aaf1e0af0a4ce848122ac5e8bb0ba7d50d059df9
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-09-06 13:34:01 +0000

    revision fix

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,4 +1,4 @@
 #define DATE_STR "06.09.2010"
-#define TIME_STR "16:26:22"
-#define SUB_VERS 2512-erez
-#define SVN_VERS "trunk-r101"
+#define TIME_STR "16:33:47"
+#define SUB_VERS 2513-erez
+#define SVN_VERS "trunk-r103"
