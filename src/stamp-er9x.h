78781afd5c018311c232553069aded86d4eb86f6
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-09-07 10:15:16 +0000

    Issue 88, Issue 89, JETI menu fix

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,4 +1,4 @@
 #define DATE_STR "07.09.2010"
-#define TIME_STR "09:44:17"
-#define SUB_VERS 2547-erez
-#define SVN_VERS "trunk-r110"
+#define TIME_STR "13:14:35"
+#define SUB_VERS 2567-erez
+#define SVN_VERS "trunk-r115"
