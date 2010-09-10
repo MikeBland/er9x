5117d4dd3502809155678ccec88d4e5f1829fd15
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-09-10 06:17:31 +0000

    Variuos speedups and cleanups

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,4 +1,4 @@
 #define DATE_STR "10.09.2010"
-#define TIME_STR "07:52:20"
-#define SUB_VERS 2616-erez
-#define SVN_VERS "trunk-r122"
+#define TIME_STR "08:55:12"
+#define SUB_VERS 2625-erez
+#define SVN_VERS "trunk-r123"
