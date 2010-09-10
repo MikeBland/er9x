6b001fd00309f42e3c2f1416f43d2672f2649972
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-09-10 15:07:10 +0000

    nother Slow fix

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,4 +1,4 @@
 #define DATE_STR "10.09.2010"
-#define TIME_STR "10:49:24"
-#define SUB_VERS 2643-erez
-#define SVN_VERS "trunk-r124"
+#define TIME_STR "18:06:49"
+#define SUB_VERS 2650-erez
+#define SVN_VERS "trunk-r125"
