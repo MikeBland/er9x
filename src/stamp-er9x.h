dba8e9455c0f4da64cc629b002250803ddd5afe6
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-09-14 23:23:32 +0000

    Issue 104

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,4 +1,4 @@
-#define DATE_STR "14.09.2010"
-#define TIME_STR "13:15:44"
-#define SUB_VERS 2716-erez
-#define SVN_VERS "trunk-r138"
+#define DATE_STR "15.09.2010"
+#define TIME_STR "01:20:43"
+#define SUB_VERS 2734-erez
+#define SVN_VERS "trunk-r139"
