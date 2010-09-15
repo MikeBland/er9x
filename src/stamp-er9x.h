54e836a4367db4207f8cfc0c7d6571fa4546311d
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-09-15 13:14:30 +0000

    New Makefile - still working on it!

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,4 +1,4 @@
 #define DATE_STR "15.09.2010"
-#define TIME_STR "01:20:43"
-#define SUB_VERS 2734-erez
-#define SVN_VERS "trunk-r139"
+#define TIME_STR "15:03:14"
+#define SUB_VERS -
+#define SVN_VERS "trunk-139"
