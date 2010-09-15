5d39c4110f9d699e92bdd495e4c52b36ff39cc8c
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-09-15 17:16:46 +0000

    Issue 105, makefile changes

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
 #define DATE_STR "15.09.2010"
-#define TIME_STR "19:07:01"
-#define SUB_VERS 2761-erez
-#define SVN_VERS "trunk-r143"
+#define TIME_STR "19:07:22"
+#define SUB_VERS 2764-erez
+#define SVN_VERS "trunk-r144"
