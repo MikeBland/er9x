a5185282d26a768d2948d12db9147fb09496ab24
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-09-15 15:12:21 +0000

    Working on Makefile

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,4 +1,5 @@
+//Automatically generated file - do not edit
 #define DATE_STR "15.09.2010"
-#define TIME_STR "15:03:01"
-#define SUB_VERS -
-#define SVN_VERS "trunk-140"
+#define TIME_STR "16:04:47"
+#define SUB_VERS 2800-erez
+#define SVN_VERS "trunk-r142"
