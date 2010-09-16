664a1487bd283064f679bb3f46a7ae976a325fd2
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-09-16 18:43:57 +0000

    limit modification - offset before limit

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
 #define DATE_STR "16.09.2010"
-#define TIME_STR "07:07:26"
-#define SUB_VERS 2767-erez
-#define SVN_VERS "trunk-r144"
+#define TIME_STR "20:08:29"
+#define SUB_VERS 2777-erez
+#define SVN_VERS "trunk-r151"
