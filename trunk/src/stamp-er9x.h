2fde130ac1eeecef125e4987da1fc70abecb0fe2
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-09-18 21:21:16 +0000

    removed copy trim from model setup.  Still available via limits

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
 #define DATE_STR "18.09.2010"
-#define TIME_STR "11:11:18"
-#define SUB_VERS 2827-erez
-#define SVN_VERS "trunk-r157"
+#define TIME_STR "23:11:22"
+#define SUB_VERS 2829-erez
+#define SVN_VERS "trunk-r161"
