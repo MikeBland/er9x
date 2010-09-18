be70c84a9c6bb939b2ed5ace1b5436d23382c372
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-09-18 04:48:05 +0000

    Issue 110

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
-#define DATE_STR "17.09.2010"
-#define TIME_STR "09:09:52"
-#define SUB_VERS 2787-erez
-#define SVN_VERS "trunk-r154"
+#define DATE_STR "18.09.2010"
+#define TIME_STR "06:06:47"
+#define SUB_VERS 2789-erez
+#define SVN_VERS "trunk-r155"
