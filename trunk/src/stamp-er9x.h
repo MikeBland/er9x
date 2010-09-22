75108a6ecde2a4a522544980e194c9e2859aac18
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-09-22 13:30:25 +0000

    beeps with pot turn

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
 #define DATE_STR "22.09.2010"
-#define TIME_STR "15:03:48"
-#define SUB_VERS 2840-erez
-#define SVN_VERS "trunk-r166"
+#define TIME_STR "15:03:05"
+#define SUB_VERS 2843-erez
+#define SVN_VERS "trunk-r167"
