7c22b179cca48e0705a1c1e8ee72696bd7d6ce5d
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-09-30 20:29:18 +0000

    template fix

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
 #define DATE_STR "30.09.2010"
-#define TIME_STR "17:05:16"
-#define SUB_VERS 2966-erez
-#define SVN_VERS "trunk-r184"
+#define TIME_STR "22:10:06"
+#define SUB_VERS 2970-erez
+#define SVN_VERS "trunk-r185"
