bc4827376b195b5d0b7b54f900140617326f4b3b
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-10-05 19:58:20 +0000

    moved RETA menu item to templates

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
 #define DATE_STR "05.10.2010"
-#define TIME_STR "21:09:20"
-#define SUB_VERS 3002-erez
-#define SVN_VERS "trunk-r190"
+#define TIME_STR "21:09:48"
+#define SUB_VERS 3011-erez
+#define SVN_VERS "trunk-r191"
