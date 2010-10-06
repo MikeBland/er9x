b5a949e79b8c732bf4ca7ef2fa182fd813d892dd
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-10-06 18:12:57 +0000

    issue 128

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
-#define DATE_STR "05.10.2010"
-#define TIME_STR "21:09:48"
-#define SUB_VERS 3011-erez
-#define SVN_VERS "trunk-r191"
+#define DATE_STR "06.10.2010"
+#define TIME_STR "20:08:41"
+#define SUB_VERS 3015-erez
+#define SVN_VERS "trunk-r193"
