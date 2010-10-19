33c9599ee933dffa90692a0e6317d5a5de6257a6
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-10-19 21:28:17 +0000

    Issue 138

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
 #define DATE_STR "19.10.2010"
-#define TIME_STR "23:11:42"
-#define SUB_VERS 3167-erez
-#define SVN_VERS "trunk-r218"
+#define TIME_STR "23:11:48"
+#define SUB_VERS 3172-erez
+#define SVN_VERS "trunk-r219"
