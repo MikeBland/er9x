65b753c8b1ff19f118db68c0a0ab004a63a0e94e
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-11-28 22:22:07 +0000

    Issue 166, Issue 174

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
-#define DATE_STR "16.11.2010"
-#define TIME_STR "07:07:31"
-#define SUB_VERS 3632-erez
-#define SVN_VERS "trunk-r267"
+#define DATE_STR "29.11.2010"
+#define TIME_STR "00:12:44"
+#define SUB_VERS 3636-erez
+#define SVN_VERS "trunk-r269"
