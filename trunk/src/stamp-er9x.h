76bc40716fece209a618cdbd6eaea08672e390d4
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-12-15 04:19:05 +0000

    small fix

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
 #define DATE_STR "15.12.2010"
-#define TIME_STR "05:05:57"
-#define SUB_VERS 3775-erez
-#define SVN_VERS "trunk-r283"
+#define TIME_STR "06:06:06"
+#define SUB_VERS 3785-erez
+#define SVN_VERS "trunk-r284"
