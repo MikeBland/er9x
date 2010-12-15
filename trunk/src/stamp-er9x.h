737e4a9ae48ff4effb7798b886e0d6d4ed3695a5
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-12-15 03:58:06 +0000

    new splash screen

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
-#define DATE_STR "12.12.2010"
-#define TIME_STR "19:07:53"
-#define SUB_VERS 3770-erez
-#define SVN_VERS "trunk-r280"
+#define DATE_STR "15.12.2010"
+#define TIME_STR "05:05:57"
+#define SUB_VERS 3775-erez
+#define SVN_VERS "trunk-r283"
