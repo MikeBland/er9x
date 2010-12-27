0c53d10e65b7d8cda6a59358eb36133b20a32ad2
erezraviv@gmail.com <erezraviv@gmail.com@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2010-12-27 10:05:42 +0000

    Heli Stuff

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
-#define DATE_STR "23.12.2010"
-#define TIME_STR "19:07:56"
-#define SUB_VERS 3866-erez
-#define SVN_VERS "trunk-r293"
+#define DATE_STR "27.12.2010"
+#define TIME_STR "12:12:40"
+#define SUB_VERS 3871-erez
+#define SVN_VERS "trunk-r1"
