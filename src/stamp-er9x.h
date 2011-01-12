116dff2b0690714df092c20917bc5daba9ae3785
erezraviv <erezraviv@gmail.com>
2011-01-12 12:41:04 +0000

    Issue 225, again :)

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
-#define DATE_STR "10.01.2011"
-#define TIME_STR "20:08:02"
-#define SUB_VERS 3969-erez
-#define SVN_VERS "trunk-r313"
+#define DATE_STR "12.01.2011"
+#define TIME_STR "14:02:03"
+#define SUB_VERS 3975-root
+#define SVN_VERS "trunk-r315"
