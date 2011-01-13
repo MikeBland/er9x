57c73b2a22994181dfc94863e8276a8f486a063c
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2011-01-13 17:13:08 +0000

    Issue 226

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
 #define DATE_STR "13.01.2011"
-#define TIME_STR "10:10:42"
-#define SUB_VERS 3983-erez
-#define SVN_VERS "trunk-r316"
+#define TIME_STR "19:07:31"
+#define SUB_VERS 3995-erez
+#define SVN_VERS "trunk-r317"
