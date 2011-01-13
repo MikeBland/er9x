dd8e213d96e102e4f77bec22650eb9ea3b108ce5
erezraviv <erezraviv@gmail.com>
2011-01-13 08:26:24 +0000

    FRSky updates

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
-#define DATE_STR "12.01.2011"
-#define TIME_STR "14:02:03"
-#define SUB_VERS 3975-root
-#define SVN_VERS "trunk-r315"
+#define DATE_STR "13.01.2011"
+#define TIME_STR "10:10:42"
+#define SUB_VERS 3983-erez
+#define SVN_VERS "trunk-r316"
