d5757c7e192aa3a9688f9fddfab26c674294a68a
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2011-01-15 14:41:02 +0000

    Swash ring fix

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
 #define DATE_STR "15.01.2011"
-#define TIME_STR "15:03:41"
-#define SUB_VERS 4003-erez
-#define SVN_VERS "trunk-r318"
+#define TIME_STR "16:04:47"
+#define SUB_VERS 4021-erez
+#define SVN_VERS "trunk-r319"
