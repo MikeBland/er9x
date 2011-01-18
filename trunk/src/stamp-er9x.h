39d24303de2f8eae515d93a8b2de3205f292c291
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2011-01-18 18:15:28 +0000

    Issue 232

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
 #define DATE_STR "18.01.2011"
-#define TIME_STR "05:05:20"
-#define SUB_VERS 4037-erez
-#define SVN_VERS "trunk-r321"
+#define TIME_STR "20:08:02"
+#define SUB_VERS 4043-erez
+#define SVN_VERS "trunk-r322"
