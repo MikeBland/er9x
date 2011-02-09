7ef2b74aa43b6b18143b97a753fafd68fe844f66
erezraviv@gmail.com <erezraviv@gmail.com@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2011-02-09 11:44:34 +0000

    Frsky update

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
-#define DATE_STR "29.01.2011"
-#define TIME_STR "21:09:54"
-#define SUB_VERS 4047-erez
-#define SVN_VERS "trunk-r326"
+#define DATE_STR "09.02.2011"
+#define TIME_STR "13:01:02"
+#define SUB_VERS 4051-erez
+#define SVN_VERS "trunk-r332"
