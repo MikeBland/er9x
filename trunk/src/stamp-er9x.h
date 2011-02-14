c6d1b867b36e4e4d517fd464bd26a4e691d90a66
erezraviv@gmail.com <erezraviv@gmail.com@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2011-02-14 09:42:48 +0000

    FRSKY/JETI switch check (reversing issue 166)

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
-#define DATE_STR "09.02.2011"
-#define TIME_STR "13:01:02"
-#define SUB_VERS 4051-erez
-#define SVN_VERS "trunk-r332"
+#define DATE_STR "14.02.2011"
+#define TIME_STR "11:11:11"
+#define SUB_VERS 4056-erez
+#define SVN_VERS "trunk-r333"
