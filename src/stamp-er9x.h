34b6fdacbd3ce320fb8931e40885020a6c3612ec
erezraviv <erezraviv@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2011-03-07 05:14:30 +0000

    Fixes

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
-#define DATE_STR "03.03.2011"
-#define TIME_STR "09:09:33"
-#define SUB_VERS 4061-erez
+#define DATE_STR "07.03.2011"
+#define TIME_STR "07:07:26"
+#define SUB_VERS 4072-erez
 #define SVN_VERS "trunk-r341"
