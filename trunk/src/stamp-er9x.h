d2d432ab62630bae5c9ae09866a4ffee8cc91b96
erezraviv@gmail.com <erezraviv@gmail.com@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2011-06-04 01:00:49 +0000

    menus\.h

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
 #define DATE_STR "03.06.2011"
-#define TIME_STR "15:03:28"
-#define SUB_VERS 4099-erez
+#define TIME_STR "20:08:52"
+#define SUB_VERS 4103-erez
 #define SVN_VERS "trunk-r352"
