b06a98697926760d48212259839db1067fdae327
erezraviv <erezraviv@gmail.com>
2011-06-23 23:32:53 +0000

    Issue 284

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
-#define DATE_STR "21.06.2011"
-#define TIME_STR "07:07:16"
-#define SUB_VERS 4167-erez
-#define SVN_VERS "trunk-r362"
+#define DATE_STR "23.06.2011"
+#define TIME_STR "19:07:38"
+#define SUB_VERS 4177-erez
+#define SVN_VERS "trunk-r363"
