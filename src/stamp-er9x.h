510b3c641c9c1bd037629370dfe08742b3fc62d2
erezraviv <erezraviv@gmail.com>
2011-07-07 01:44:17 +0000

    nother cleanup

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
 #define DATE_STR "06.07.2011"
-#define TIME_STR "21:09:38"
-#define SUB_VERS 4228-erez
-#define SVN_VERS "trunk-r368"
+#define TIME_STR "21:09:04"
+#define SUB_VERS 4233-erez
+#define SVN_VERS "trunk-r369"
