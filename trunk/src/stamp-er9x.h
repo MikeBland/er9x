3d1810dbf4d210212eb8dac1870d18e735a803e9
erezraviv@gmail.com <erezraviv@gmail.com@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2011-07-19 02:31:47 +0000

    scrolling fixes

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
 #define DATE_STR "18.07.2011"
-#define TIME_STR "21:09:43"
-#define SUB_VERS 4439-erez
-#define SVN_VERS "trunk-r378"
+#define TIME_STR "22:10:20"
+#define SUB_VERS 4470-erez
+#define SVN_VERS "trunk-r379"
