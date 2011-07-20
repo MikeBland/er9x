f6f3722610c148595b800f616cde38c36ee76ac5
erezraviv <erezraviv@gmail.com>
2011-07-20 01:18:38 +0000

    Name edid with pot scroll

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
-#define DATE_STR "18.07.2011"
-#define TIME_STR "22:10:20"
-#define SUB_VERS 4470-erez
-#define SVN_VERS "trunk-r379"
+#define DATE_STR "19.07.2011"
+#define TIME_STR "21:09:02"
+#define SUB_VERS 4488-erez
+#define SVN_VERS "trunk-r380"
