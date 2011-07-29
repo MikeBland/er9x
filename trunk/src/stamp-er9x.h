d12e5597e7cb721cf404b4d12c655c2fbf381efa
erezraviv@gmail.com <erezraviv@gmail.com@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2011-07-29 02:55:01 +0000

    MAX back to what it was...

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,5 +1,5 @@
 //Automatically generated file - do not edit
 #define DATE_STR "28.07.2011"
-#define TIME_STR "21:09:34"
-#define SUB_VERS 4549-erez
-#define SVN_VERS "trunk-r384"
+#define TIME_STR "22:10:29"
+#define SUB_VERS 4558-erez
+#define SVN_VERS "trunk-r385"
