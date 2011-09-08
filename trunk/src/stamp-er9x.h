7c9f2487a2d1de59a15ab9d93ed4091aa382e15d
erezraviv@gmail.com <erezraviv@gmail.com@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2011-09-08 01:33:25 +0000

    NMEA doc and version information on screen

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "23.08.2011"
-#define TIME_STR "21:09:49"
-#define SUB_VERS 4765-erez
-#define SVN_VERS "trunk-r467"
-#define MOD_VERS "STD"
+#define DATE_STR "07.09.2011"
+#define TIME_STR "21:09:59"
+#define SUB_VERS 4777-erez
+#define SVN_VERS "trunk-r517"
+#define MOD_VERS "   "
