d52633025e44c835335b7817b4c838c3b57b217b
erezraviv <erezraviv@gmail.com>
2011-09-17 23:39:00 +0000

    Frsky Fix

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "16.09.2011"
-#define TIME_STR "10:10:27"
-#define SUB_VERS 4800-erez
-#define SVN_VERS "trunk-r535"
+#define DATE_STR "17.09.2011"
+#define TIME_STR "19:07:40"
+#define SUB_VERS 4811-erez
+#define SVN_VERS "trunk-r537"
 #define MOD_VERS "   "
