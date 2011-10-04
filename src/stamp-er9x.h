1239542969676ac536ffc4e5fee6e96e636d8e7e
erezraviv <erezraviv@gmail.com>
2011-10-04 03:06:30 +0000

    calibration screen update

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
 #define DATE_STR "03.10.2011"
-#define TIME_STR "20:08:16"
-#define SUB_VERS 4949-erez
-#define SVN_VERS "trunk-r554"
+#define TIME_STR "22:10:07"
+#define SUB_VERS 4962-erez
+#define SVN_VERS "trunk-r555"
 #define MOD_VERS "   "
