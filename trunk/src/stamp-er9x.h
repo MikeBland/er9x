c4a469a127cfde61684a553a0e892bf97c048cdd
erezraviv@gmail.com <erezraviv@gmail.com@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2011-10-06 02:27:12 +0000

    instatrim fix for helis

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
 #define DATE_STR "05.10.2011"
-#define TIME_STR "18:06:14"
-#define SUB_VERS 5006-erez
-#define SVN_VERS "trunk-r567"
+#define TIME_STR "22:10:46"
+#define SUB_VERS 5029-erez
+#define SVN_VERS "trunk-r568"
 #define MOD_VERS "   "
