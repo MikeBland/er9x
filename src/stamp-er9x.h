2bffa817ad02f2f65a57d2b749e9e8492b562ae4
erezraviv <erezraviv@gmail.com>
2011-10-09 22:30:41 +0000

    NMEA update

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "07.10.2011"
-#define TIME_STR "16:04:12"
-#define SUB_VERS 5051-erez
-#define SVN_VERS "trunk-r572"
+#define DATE_STR "09.10.2011"
+#define TIME_STR "18:06:12"
+#define SUB_VERS 5062-erez
+#define SVN_VERS "trunk-r573"
 #define MOD_VERS "   "
