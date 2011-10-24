aabcbb87e7051f983ca39d80d6a66c32c56532d9
erezraviv <erezraviv@gmail.com>
2011-10-24 18:16:46 +0000

    Various changes

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "09.10.2011"
-#define TIME_STR "18:06:12"
-#define SUB_VERS 5062-erez
-#define SVN_VERS "trunk-r573"
+#define DATE_STR "24.10.2011"
+#define TIME_STR "14:02:51"
+#define SUB_VERS 5073-erez
+#define SVN_VERS "trunk-r577"
 #define MOD_VERS "   "
