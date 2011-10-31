999000f7658780598daf9cecd53395cc4cf65d24
erezraviv@gmail.com <erezraviv@gmail.com@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2011-10-31 21:35:30 +0000

    timer fix

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "28.10.2011"
-#define TIME_STR "19:07:12"
-#define SUB_VERS 5084-erez
-#define SVN_VERS "trunk-r578"
+#define DATE_STR "31.10.2011"
+#define TIME_STR "17:05:10"
+#define SUB_VERS 5095-erez
+#define SVN_VERS "trunk-r582"
 #define MOD_VERS "   "
