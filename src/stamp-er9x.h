c9a28709478fd098a88fed5a08e814cc36dbb349
erezraviv <erezraviv@gmail.com>
2011-11-14 01:11:59 +0000

    build of last change

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "09.11.2011"
-#define TIME_STR "22:10:51"
-#define SUB_VERS 5147-erez
-#define SVN_VERS "trunk-r607"
+#define DATE_STR "13.11.2011"
+#define TIME_STR "17:05:09"
+#define SUB_VERS 5154-erez
+#define SVN_VERS "trunk-r621"
 #define MOD_VERS "   "
