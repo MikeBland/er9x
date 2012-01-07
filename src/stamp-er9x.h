11b3ec80d25aadb5e55da0e2f341353176d5cd63
erezraviv <erezraviv@gmail.com>
2012-01-07 21:47:18 +0000

    FrSky telemetry fix

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
 #define DATE_STR "07.01.2012"
-#define TIME_STR "04:04:59"
-#define SUB_VERS 5241-erez
-#define SVN_VERS "trunk-r653"
+#define TIME_STR "16:04:32"
+#define SUB_VERS 5248-erez
+#define SVN_VERS "trunk-r654"
 #define MOD_VERS "   "
