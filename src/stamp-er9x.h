1d333a48931d1c1b593bdeb4e041055d22456a1a
erezraviv <erezraviv@gmail.com>
2012-01-28 18:06:17 +0000

    Frsky version will now be without templates - sorry

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
 #define DATE_STR "28.01.2012"
-#define TIME_STR "13:01:20"
-#define SUB_VERS 5437-erez
-#define SVN_VERS "trunk-r688"
-#define MOD_VERS "FRSKY NOT  "
+#define TIME_STR "13:01:56"
+#define SUB_VERS 5444-erez
+#define SVN_VERS "trunk-r690"
+#define MOD_VERS "   "
