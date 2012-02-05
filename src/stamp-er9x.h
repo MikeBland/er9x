75a2b443ea81f23f27c2f2bbc5520f2bc575c15a
erezraviv <erezraviv@gmail.com>
2012-02-05 02:13:48 +0000

    build

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "02.02.2012"
-#define TIME_STR "20:08:23"
-#define SUB_VERS 5451-erez
-#define SVN_VERS "er9x-r699"
+#define DATE_STR "04.02.2012"
+#define TIME_STR "21:09:47"
+#define SUB_VERS 5465-erez
+#define SVN_VERS "er9x-r701"
 #define MOD_VERS "   "
