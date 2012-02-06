d9c7dc42a70687d6853c4a16a1ded16bc47a9a8c
erezraviv <erezraviv@gmail.com>
2012-02-06 17:56:31 +0000

    Temp build to fix urgent error in myeeprom.h

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "04.02.2012"
-#define TIME_STR "21:09:47"
-#define SUB_VERS 5465-erez
-#define SVN_VERS "er9x-r701"
+#define DATE_STR "06.02.2012"
+#define TIME_STR "12:12:06"
+#define SUB_VERS 5479-eerr
+#define SVN_VERS "er9x-r703"
 #define MOD_VERS "   "
