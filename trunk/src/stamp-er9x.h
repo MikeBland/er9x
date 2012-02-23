199dba7c06f90b90b44e2088fb8dca482526f7f4
erezraviv@gmail.com <erezraviv@gmail.com@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2012-02-23 01:55:54 +0000

    PXX build

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
 #define DATE_STR "22.02.2012"
-#define TIME_STR "21:09:03"
-#define SUB_VERS 5516-Mike
-#define SVN_VERS "er9x-r729"
-#define MOD_VERS "FRSKY NT  "
+#define TIME_STR "20:08:26"
+#define SUB_VERS 5530-erez
+#define SVN_VERS "er9x-r732"
+#define MOD_VERS "   "
