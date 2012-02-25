c1e9880f9240dbe40faeb76a396170b95ba0cfe6
michael.blandford <michael.blandford@mypostoffice.co.uk>
2012-02-25 23:29:07 +0000

    222 more bytes flash saved

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
 #define DATE_STR "25.02.2012"
-#define TIME_STR "17:05:48"
-#define SUB_VERS 5547-erez
-#define SVN_VERS "er9x-r734"
-#define MOD_VERS "   "
+#define TIME_STR "23:11:16"
+#define SUB_VERS 5548-Mike
+#define SVN_VERS "er9x-r735"
+#define MOD_VERS "FRSKY NT  "
