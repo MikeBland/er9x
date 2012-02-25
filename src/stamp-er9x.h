171f60e8765e1c7f08481c445426f421cea73a8a
erezraviv <erezraviv@gmail.com>
2012-02-25 23:03:23 +0000

    Space saving + add confirmation while deleting mix

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "22.02.2012"
-#define TIME_STR "20:08:26"
-#define SUB_VERS 5530-erez
-#define SVN_VERS "er9x-r732"
+#define DATE_STR "25.02.2012"
+#define TIME_STR "17:05:48"
+#define SUB_VERS 5547-erez
+#define SVN_VERS "er9x-r734"
 #define MOD_VERS "   "
