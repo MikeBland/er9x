eaaa1f27760ca88d561a4a6d923b2d16c6ad06e7
erezraviv <erezraviv@gmail.com>
2012-02-26 06:47:18 +0000

    space savings build

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "25.02.2012"
-#define TIME_STR "23:11:16"
-#define SUB_VERS 5548-Mike
-#define SVN_VERS "er9x-r735"
-#define MOD_VERS "FRSKY NT  "
+#define DATE_STR "26.02.2012"
+#define TIME_STR "01:01:39"
+#define SUB_VERS 5555-erez
+#define SVN_VERS "er9x-r736"
+#define MOD_VERS "   "
