6456ec1b2d7fd481390268583d90b012e200c0e8
erezraviv@gmail.com <erezraviv@gmail.com@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2012-02-29 02:34:59 +0000

    fix pxx lockup issue when changing back to ppm

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
 #define DATE_STR "28.02.2012"
-#define TIME_STR "19:07:02"
-#define SUB_VERS 5563-erez
-#define SVN_VERS "er9x-r737"
+#define TIME_STR "21:09:46"
+#define SUB_VERS 5573-erez
+#define SVN_VERS "er9x-r739"
 #define MOD_VERS "   "
