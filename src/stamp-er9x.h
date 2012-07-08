67e33b2f6e52afee49076062a09673e6d16e3263
erezraviv <erezraviv@gmail.com>
2012-07-08 00:51:20 +0000

    build

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "14.05.2012"
-#define TIME_STR "20:50:26"
-#define SUB_VERS 5655-erez
-#define SVN_VERS "er9x-r762"
+#define DATE_STR "07.07.2012"
+#define TIME_STR "20:48:56"
+#define SUB_VERS 5662-erez
+#define SVN_VERS "er9x-r765"
 #define MOD_VERS "   "
