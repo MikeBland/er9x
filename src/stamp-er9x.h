164c188d891c06fab91cced5e0568bfb443584cb
erezraviv <erezraviv@gmail.com>
2012-07-08 13:37:12 +0000

    built with gcc 4-7-1

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "07.07.2012"
-#define TIME_STR "20:48:56"
-#define SUB_VERS 5662-erez
-#define SVN_VERS "er9x-r765"
+#define DATE_STR "08.07.2012"
+#define TIME_STR "09:36:37"
+#define SUB_VERS 5691-erez
+#define SVN_VERS "er9x-r766"
 #define MOD_VERS "   "
