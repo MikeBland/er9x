cf2c24da8221664e71963d86f55e59972fb72caa
michael.blandford <michael.blandford@mypostoffice.co.uk>
2012-08-14 15:56:34 +0000

    Fix PPM problem

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "13.08.2012"
-#define TIME_STR "22:17:37"
-#define SUB_VERS 5764-Mike
-#define SVN_VERS "er9x-r770"
-#define MOD_VERS "FRSKY NT  "
+#define DATE_STR "14.08.2012"
+#define TIME_STR "15:41:19"
+#define SUB_VERS 5776-Mike
+#define SVN_VERS "er9x-r771"
+#define MOD_VERS "   "
