c7b728ef0d82c6b11684d24b95ebb671d957c40b
erezraviv@gmail.com <erezraviv@gmail.com@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2013-03-04 13:06:04 +0000

    build

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
 #define DATE_STR "04.03.2013"
-#define TIME_STR "07:47:58"
-#define SUB_VERS 179-erez
-#define SVN_VERS "er9x-r784"
-#define MOD_VERS "    "
+#define TIME_STR "08:05:50"
+#define SUB_VERS 189-erez
+#define SVN_VERS "er9x-r785"
+#define MOD_VERS "FRSKY   128 "
