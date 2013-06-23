4215a0edced79c995040fd36cb4b244fcb25c692
michael.blandford <michael.blandford@mypostoffice.co.uk>
2013-06-23 18:35:35 +0000

    Fix templates display bug

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "22.06.2013"
-#define TIME_STR "20:42:37"
-#define SUB_VERS 1025-Mike
-#define SVN_VERS "er9x-r796"
+#define DATE_STR "23.06.2013"
+#define TIME_STR "18:30:21"
+#define SUB_VERS 1041-Mike
+#define SVN_VERS "er9x-r797"
 #define MOD_VERS "FRSKY NT    "
