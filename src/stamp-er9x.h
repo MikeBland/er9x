0ef22ebd077c6fa47a6f7bbbea1a7d3f54d03e69
michael.blandford <michael.blandford@mypostoffice.co.uk>
2013-10-15 10:29:02 +0000

    Fix EXPO/DR bug, Trim improvements

--- src/stamp-er9x.h
+++ src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "13.10.2013"
-#define TIME_STR "08:57:55"
-#define SUB_VERS 1998-Mike
-#define SVN_VERS "er9x-r805"
+#define DATE_STR "15.10.2013"
+#define TIME_STR "10:19:26"
+#define SUB_VERS 2034-Mike
+#define SVN_VERS "er9x-r806"
 #define MOD_VERS "FRSKY NT    "
