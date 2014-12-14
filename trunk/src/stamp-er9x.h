137f62e18005164d673fc214aab348c8aeb56991
michael.blandford@mypostoffice.co.uk <michael.blandford@mypostoffice.co.uk@5d977f70-4713-f8a9-b4ee-1a69ca267c72>
2014-12-14 11:20:28 +0000

    Fix bugs - collective, beep centre and mix when channel is its own source

--- trunk/src/stamp-er9x.h
+++ trunk/src/stamp-er9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "16.11.2014"
-#define TIME_STR "23:22:03"
-#define SUB_VERS 401-Mike
-#define SVN_VERS "er9x-r814"
+#define DATE_STR "14.12.2014"
+#define TIME_STR "11:14:12"
+#define SUB_VERS 429-Mike
+#define SVN_VERS "er9x-r815"
 #define MOD_VERS "FRSKY NT    "
